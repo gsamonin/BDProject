f4a288bd7eeb6796fc922bdc771c28166a9a8efe
Samoni George <gsamonin@mail.ru>
2022-11-21 10:31:12 +0300

    Final 1.0

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, url_for, request
+from flask_mysqldb import MySQL
+import datetime
+
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '3463159q'
+app.config['MYSQL_DB'] = 'gsamonin'
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    dn = datetime.date
+    if request.method == "POST":
+        guestsDetails = request.form
+        firstname = guestsDetails['firstname']
+        secondname = guestsDetails['secondname']
+        phone = guestsDetails['phone']
+        email = guestsDetails['email']
+        bil = guestsDetails['bil']
+        amountPer = guestsDetails['amountPer']
+        time_nach = guestsDetails['time_nach']
+        time_okon = guestsDetails['time_okon']
+        date_game = guestsDetails['date_game']
+        if date_game < str(dn):
+            return "Вы не можете играть в прошлом. " \
+                   "Самое страшное — когда прошлым становятся те, кто должен был стать будущим."
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO guests(firstname, secondname, "
+                    "phone, email) VALUES(%s, %s, %s, %s)", (firstname, secondname, phone, email))
+        mysql.connection.commit()
+        cur.execute("SELECT * FROM guests WHERE phone like %s", [phone])
+        person = cur.fetchone()
+        id = person[0]
+        cur.execute("INSERT INTO game(guest_idgame, bil_id, amount, time_nach, time_okon, date_game) "
+                    "VALUES(%s, %s, %s, %s, %s, %s)", (id, bil, amountPer, time_nach, time_okon, date_game))
+        mysql.connection.commit()
+        cur.close()
+    return render_template("index.html")
+
+
+@app.route('/menu', methods=['GET', 'POST'])
+def menu():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM menu where NOT id_menu = 10")
+    data = cur.fetchall()
+    cur.close
+    print(data)
+    return render_template("menu.html", menu = data)
+
+
+@app.route('/price')
+def price():
+    return render_template("price.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
